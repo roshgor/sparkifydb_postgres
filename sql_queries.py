16fbd6627250dff05da0c45f8116e3452c03d65d
Roshan Joseph George <roshan.j.george@gmail.com>
2020-08-12 20:06:31 +0530

    v3

--- sql_queries.py
+++ sql_queries.py
@@ -77,7 +77,7 @@ songplay_table_insert = (""" INSERT INTO songplays(start_time, user_id, level, s
 """)
 
 user_table_insert = (""" INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) 
-    ON CONFLICT (user_id) DO UPDATE SET level= EXCLUDED.level WHERE level = 'free' ;
+    ON CONFLICT (user_id) DO UPDATE SET level= EXCLUDED.level WHERE users.level = 'free' ;
 """)
 
 song_table_insert = (""" INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) 
