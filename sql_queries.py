6b1ade3b2a8219a76c76e6f61420cb7aec20152f
Roshan Joseph George <roshan.j.george@gmail.com>
2020-08-12 19:55:23 +0530

    v2

--- sql_queries.py
+++ sql_queries.py
@@ -62,7 +62,7 @@ user_agent varchar
 # start_time, hour, day, week, month, year, weekday
 time_table_create = (""" 
 CREATE TABLE IF NOT EXISTS time(
-start_time timestamp, 
+start_time timestamp PRIMARY KEY, 
 hour int, 
 day int, 
 week int, 
@@ -76,7 +76,8 @@ weekday varchar);
 songplay_table_insert = (""" INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES(%s, %s, %s, %s, %s, %s, %s, %s) ;
 """)
 
-user_table_insert = (""" INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;
+user_table_insert = (""" INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) 
+    ON CONFLICT (user_id) DO UPDATE SET level= EXCLUDED.level WHERE level = 'free' ;
 """)
 
 song_table_insert = (""" INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) 
@@ -86,7 +87,8 @@ ON CONFLICT (song_id) DO NOTHING ;
 artist_table_insert = (""" INSERT INTO artists(artist_id, name, location, latitude, longitude) values (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING ;
 """)
 
-time_table_insert = (""" INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ;
+time_table_insert = (""" INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) 
+    ON CONFLICT (start_time) DO NOTHING ;
 """)
 
 # FIND SONGS
